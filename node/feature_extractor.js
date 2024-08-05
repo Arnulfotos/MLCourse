dc05afd56bdfe25f165a19381b34a94ee903de6d
Arnulfo Leon <manuelleon1996@gmail.com>
2024-08-05 00:47:23 -0700

    El triatlon lo ganaron los teutones

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,25 +1,33 @@
-const constants = require("../common/constants.js")
-const featureFunctions = require("../common/featureFunctions.js")
+const { features } = require("process");
+const constants = require("../common/constants.js");
+const featureFunctions = require("../common/featureFunctions.js");
 
 const fs = require("fs");
-console.log("EXTRACTING FEATURES...")
+console.log("EXTRACTING FEATURES...");
 
-const samples = JSON.parse(fs.readFileSync(constants.SAMPLES))
+const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
 
 for (const sample of samples) {
-    const paths = JSON.parse(fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json"))
-
-    sample.point = [featureFunctions.getPathCount(paths), featureFunctions.getPointCount(paths)]
-
-
-
-
+  const paths = JSON.parse(
+    fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json")
+  );
+  const functions = featureFunctions.inUse.map((f) => f.function);
+  sample.point = functions.map((f) => f(paths));
 }
 
+const featureNames = featureFunctions.inUse.map((f) => f.name);
+fs.writeFileSync(
+  constants.FEATURES,
+  JSON.stringify({
+    featureNames,
+    samples: samples.map((s) => {
+      return { point: s.point, label: s.label };
+    }),
+  })
+);
 
-const featureNames = ["Path Count", "Point Count"];
-fs.writeFileSync(constants.FEATURES, JSON.stringify({ featureNames, samples: samples.map(s => { return { point: s.point, label: s.label } }) }))
-
-fs.writeFileSync(constants.FEATURES_JS, `const features = ${JSON.stringify({ featureNames, samples })};`)
-console.log("EXCTRACTION FINISHED")
-
+fs.writeFileSync(
+  constants.FEATURES_JS,
+  `const features = ${JSON.stringify({ featureNames, samples })};`
+);
+console.log("EXCTRACTION FINISHED");
